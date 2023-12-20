f4a3eb95313f4676ad33c041fd00638a4c7b3d74
foilest-123 <thegreatvijay1991@gmail.com>
2023-12-20 17:55:16 +0530

    Step 6 commit

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
-from flask import Flask , render_template , request , jsonify
+from flask import Flask , render_template , request , jsonify, redirect , url_for
 import psycopg2 
 from sqlalchemy import create_engine
-
-
+from flask_sqlalchemy import SQLAlchemy 
 app = Flask(__name__)
 
 @app.route("/")
@@ -13,25 +12,27 @@ conn = psycopg2.connect(database="sample_test",
                         user="postgres", 
                     password="1234",  
                     host="localhost", port="5432") 
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:1234@localhost/sample_test'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Optional but recommended for performance
+
+db = SQLAlchemy(app)
+
+
 @app.route("/connect_psql" , methods =["POST" , "GET"])
 def sql_connection():
-
     cur = conn.cursor() 
     cur.execute('''select * from students''')
-
-    data = cur.fetchall()
-       
+    data = cur.fetchall()   
     conn.commit() 
     cur.close() 
     conn.close()
-
     return render_template('index.html' , data=data)
 
 
 @app.route("/addnumber")
 def add_number():
     return render_template('add.html')
-
 
 @app.route("/addnumber", methods=["POST"])
 def calculate_sum():
@@ -58,58 +59,95 @@ def bankdata():
         return jsonify({"error": "Invalid JSON format or net_amount not found"})
     
 
+class Employee(db.Model):
+    __table_name__ = 'employee'
+    employee_id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(50))
+    last_name = db.Column(db.String(50))
+    email = db.Column(db.String(100))
+    department = db.Column(db.String(50))
+    salary = db.Column(db.Numeric(10, 2))
+    hire_date = db.Column(db.Date)
+    def __init__(self, first_name, last_name, email, department, salary, hire_date):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.department = department
+        self.salary = salary
+        self.hire_date = hire_date
+@app.route('/employees', methods=['GET'])
+def get_employees():
+    employees = Employee.query.all()
+    result = []
+    for employee in employees:
+        result.append({
+            'id': employee.employee_id,
+            'first_name': employee.first_name,
+            'last_name': employee.last_name,
+            'email': employee.email,
+            'department': employee.department,
+            'salary': float(employee.salary),
+            'hire_date': str(employee.hire_date)
+        })
+    return render_template('/addemployees.html' , result = result)
 
-@app.route('/addemployees' , methods = ["POST" , "GET"])
-def addemployees():
-   
-    engine = create_engine('postgresql+psycopg2://postgres:1234@localhost/sample_test')
-    cur = engine.cursor()
-    cur.execute('''CREATE TABLE IF NOT EXISTS testing (
-    employee_id SERIAL PRIMARY KEY,
-    first_name VARCHAR(50),
-    last_name VARCHAR(50),
-    email VARCHAR(100) UNIQUE,
-    department VARCHAR(50),
-    salary NUMERIC(10, 2),
-    hire_date DATE
-);
-                              ''')
-    conn.commit()
+@app.route('/add_employee', methods=["POST"])
+def add_employee():
+        new_employee = Employee(
+            first_name=request.form.get('firstname'),
+            last_name=request.form.get('lastname'),
+            email=request.form.get('email'),
+            department=request.form.get('department'),
+            salary=request.form.get('salary'),
+            hire_date=request.form.get('hiredate')
+        )
+        db.session.add(new_employee)
+        db.session.commit()
+        return redirect(url_for('get_employees'))
 
-    first_name = request.form.get('firstname')
-    lastname = request.form.get('lastname')
-    email = request.form.get('email')
-    department = request.form.get('department')
-    salary = request.form.get('salary')
-    hire_date = request.form.get('hiredate')
+@app.route('/update_employee', methods=["POST"])
+def update_employee():
 
-    cur.execute('''
-    INSERT INTO Employee (first_name, last_name, email, department, salary, hire_date)
-    VALUES (%s, %s, %s, %s, %s, %s)
-''', (first_name, lastname, email, department, salary, hire_date))
+    form_data = request.form.to_dict() 
+    employee = Employee.query.filter(Employee.employee_id == form_data['id']).first()
+
+    if not employee:
+        return jsonify({'message': 'Employee not found'}), 404
+ 
+    # Filter out the keys that are not present or have empty values
+    update_data = {key: value for key, value in form_data.items()}
+    previous_value = Employee.query.get(Employee.employee_id == form_data['id']).first()
+    # Update only the provided fields
+    print(update_data)
+    for key, value in update_data.items():
+     setattr(employee, key, value)
+    # Check for empty values in updated data and copy from employee if empty
+    for key, value in update_data.items():
+       if not value:
+        setattr(employee, key, getattr(employee, key)) 
+    print(previous_value) # Copy from employee if value is empty
+    db.session.commit()
     
-
-    alldetails = cur.execute('''select * from Employee''')
-    return render_template('addemployees.html' , data = alldetails)
-    cur.close()
-    conn.close()
+    return redirect(url_for('get_employees'))
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+@app.route('/delete_employee' , methods=["POST"])
+def delete_employee():
+    search_id = request.form.get('search_id')
+    search_name = request.form.get('search_name')
+    employee_to_delete = (
+        Employee.query.filter(
+            (Employee.employee_id == search_id) |
+            (Employee.first_name == search_name) |
+            (Employee.last_name == search_name)
+        ).first()
+    )
+    if employee_to_delete:
+        db.session.delete(employee_to_delete)
+        db.session.commit()
+    return redirect(url_for('get_employees'))
 
 
 
-
-app.run(host="0.0.0.0", port="5000", debug=True)
+if __name__ == "__main__":
+    app.run()
